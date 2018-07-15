7916c1b697715e791298b39ebb6aa905dbe9c4c4
mzeng <mailworks@qq.com>
2018-07-15 17:31:13 +0800

    Hide Warning in Xcode

---
+++ Potatso/SHFTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SHFTabBarController.h
+//  Potatso
+//
+//  Created by zeng on 2018/7/15.
+//  Copyright © 2018年 TouchingApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHFTabBarController : UITabBarController
+
+@end
